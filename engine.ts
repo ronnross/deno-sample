0a2b1462aa5f06d11b277faa76dfe0588db2ebd3
Ronn Ross <ronn.ross@gmail.com>
2023-07-13 11:25:35 -0400

    adding timer from data model

--- engine.ts
+++ engine.ts
@@ -1,6 +1,7 @@
 import log from "./logger.ts";
+
 const sleep = (delay: number) =>
-  new Promise((resolve) => setTimeout(resolve, delay));
+  new Promise((resolve) => setTimeout(resolve, delay * 1000));
 
 function startGame(gameData: any) {
   const scenes = gameData;
@@ -20,8 +21,11 @@ function startGame(gameData: any) {
       Deno.exit(1);
     }
     if (result != null) {
-      log("should sleep");
-      await sleep(4000);
+      const getDelay = scenes[scene].actions.find(
+        (x: any) => x.action === result
+      );
+
+      await sleep(getDelay.delay);
       changeScene(result);
     }
   };
